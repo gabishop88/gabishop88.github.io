47ac91d94eefde61775e1102ba6e11406916e1d4
Graham Bishop <gabishop15@gmail.com>
2022-07-30 12:19:52 -0500

    testing scrollhandler

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,11 @@
-function clicktext(a) {
-    console.log(a)
+function init() {
+    // console.log("Starting Initialization")
+    document.addEventListener('scroll', (e) => scrollhandler(e));
+
+    d3.select('#canvas')
+    .append('rect').attr('x', 100).attr('y', 100).attr('height', 50).attr('width', 50)
+}
+
+function scrollhandler(event) {
+    console.log(event);
 }
