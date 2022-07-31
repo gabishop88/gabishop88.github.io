1229e1f12b8fbe3ea2cdefd310bc52fe418a92b7
Graham Bishop <gabishop15@gmail.com>
2022-07-30 23:30:47 -0500

    attempted to load data

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,19 @@
-function init() {
-    // console.log("Starting Initialization")
+async function init() {
     document.addEventListener('scroll', (e) => scrollhandler(e));
 
-    d3.select('#canvas')
-    .append('rect').attr('x', 100).attr('y', 100).attr('height', 50).attr('width', 50)
+
+    hi_income_data = await d3.csv('./data/water_and_sanitation_high_income.csv');
+    console.log(hi_income_data);
+
+    /**
+     * Layout Plan - Martini glass
+     *  - Scene 1: Scatterplot with water access versus income, shouldn't be much of a surprise, but should give contex for what I am trying to show
+     *  - Scene 2: Scatterplot with water access versus some other metric, maybe health or access to sanitation. Should narrow in on the issue or some bullshit like that
+     *  - Scene 3: Map showing all countries with data available. Color is determined by access to water, and clicking should give access to organizations to donate to.
+     */
 }
 
 function scrollhandler(event) {
-    console.log(event);
+    // event.target.preventDefault();
+    console.log(window.scrollY);
 }
